770b28aefb02b2116b11e2a7779360195129aeb0
Risto Laurikainen <risto.laurikainen@csc.fi>
2013-11-18 09:34:04 +0200

    Added support for separate public compute and EC2 ports for e.g. SSL reverse proxy

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -6,7 +6,9 @@ class nova::keystone::auth(
   $admin_address          = '127.0.0.1',
   $internal_address       = '127.0.0.1',
   $compute_port           = '8774',
+  $public_compute_port    = undef,
   $ec2_port               = '8773',
+  $public_ec2_port        = undef,
   $compute_version        = 'v2',
   $region                 = 'RegionOne',
   $tenant                 = 'services',
@@ -16,6 +18,18 @@ class nova::keystone::auth(
   $cinder                 = undef,
   $public_protocol        = 'http'
 ) {
+
+  if ! $public_compute_port {
+    $real_public_compute_port = $compute_port
+  } else {
+    $real_public_compute_port = $public_compute_port
+  }
+
+  if ! $public_ec2_port {
+    $real_public_ec2_port = $ec2_port
+  } else {
+    $real_public_ec2_port = $public_ec2_port
+  } 
 
   if $cinder != undef {
     warning('cinder parameter is deprecated and has no effect.')
@@ -43,7 +57,7 @@ class nova::keystone::auth(
   }
   keystone_endpoint { "${region}/${auth_name}":
     ensure       => present,
-    public_url   => "${public_protocol}://${public_address}:${compute_port}/${compute_version}/%(tenant_id)s",
+    public_url   => "${public_protocol}://${public_address}:${real_public_compute_port}/${compute_version}/%(tenant_id)s",
     admin_url    => "http://${admin_address}:${compute_port}/${compute_version}/%(tenant_id)s",
     internal_url => "http://${internal_address}:${compute_port}/${compute_version}/%(tenant_id)s",
   }
@@ -56,7 +70,7 @@ class nova::keystone::auth(
     }
     keystone_endpoint { "${region}/${auth_name}_ec2":
       ensure       => present,
-      public_url   => "${public_protocol}://${public_address}:${ec2_port}/services/Cloud",
+      public_url   => "${public_protocol}://${public_address}:${real_public_ec2_port}/services/Cloud",
       admin_url    => "http://${admin_address}:${ec2_port}/services/Admin",
       internal_url => "http://${internal_address}:${ec2_port}/services/Cloud",
     }
